076b31ec92698bc1208663f24ca6b28ea2748352
vincentliu130 <she4869@hotmail.com>
2017-03-28 14:10:19 -0400

    Side Menu with slide-out animation + Group profile pages :hatched_chick:

---
+++ DayDay/GroupDetails.swift
@@ -0,0 +1,19 @@
+//
+//  GroupDetails.swift
+//  DayDay
+//
+//  Created by Vincent Liu on 3/28/17.
+//  Copyright © 2017 WePlay. All rights reserved.
+//
+
+internal class GroupDetails {
+    internal let id: String
+    internal let name: String
+    internal let detail: String
+    
+    init(id: String, name: String, detail: String) {
+        self.id = id
+        self.name = name
+        self.detail = detail
+    }
+}
