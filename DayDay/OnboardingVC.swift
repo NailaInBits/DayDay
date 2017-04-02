7b7621767240468e06a266c68a5af5414006779b
vincentliu130 <she4869@hotmail.com>
2017-04-02 10:44:10 -0400

    Updated onboarding page feature :whale:

--- DayDay/OnboardingVC.swift
+++ DayDay/OnboardingVC.swift
@@ -15,13 +15,28 @@ class OnboardingVC: UIViewController {
     
     var gradientLayer: CAGradientLayer!
     
-    private var ref: FIRDatabaseReference = FIRDatabase.database().reference().child("KpopGroups")
+    private var userRef: FIRDatabaseReference = FIRDatabase.database().reference().child("users")
     private var values = [String]()
     let userID = FIRAuth.auth()?.currentUser?.uid
-    private var kpopGroups: [KpopGroup] = []
-    private var userRef: FIRDatabaseReference = FIRDatabase.database().reference().child("users")
-
     
+    private var buttonSelected: [String: Bool] = [
+                                         "bts": false,
+                                         "blackPink": false,
+                                         "exo": false,
+                                         "twice": false,
+                                         "bigBang": false,
+                                         "got7": false
+                                         ]
+    
+    private var groupIDs: [String: String] = [
+                          "bts": "-KgCx7qeem3u2dlMDr0i",
+                          "blackPink": "-KgCx8cRgj4qca_K4aBR",
+                          "exo": "-KgCx9jdmLA78msqYnV-",
+                          "twice": "-KgH9pgaNoBpu_wYqV8o",
+                          "bigBang": "-KgH9qIca2Wgw-Dhs4JS",
+                          "got7": "-KgH9qbONq9hzFQcagPJ"
+                          ]
+
     @IBOutlet var instructionsLabel: UILabel!
     @IBOutlet var firstButton: UIButton!
     @IBOutlet var secondButton: UIButton!
@@ -73,7 +88,15 @@ class OnboardingVC: UIViewController {
     }
     
     @IBAction func addGroup1(_ sender: UIButton) {
-        ref = FIRDatabase.database().reference()
+        if buttonSelected["bts"]! {
+            firstButton.setBackgroundImage(#imageLiteral(resourceName: "g1"), for: UIControlState.normal)
+            buttonSelected["bts"] = false
+        } else {
+            firstButton.setBackgroundImage(#imageLiteral(resourceName: "check"), for: UIControlState.normal)
+            firstButton.backgroundColor = UIColor.clear
+            buttonSelected["bts"] = true
+        }
+        /*ref = FIRDatabase.database().reference()
         
         let kpopGroupRef = ref.child("users/" + userID! + "/kpopGroups")
         
@@ -90,12 +113,20 @@ class OnboardingVC: UIViewController {
             } else {
                 print("Error! Could not decode group data")
             }
-        })
+        })*/
 
     }
     
     @IBAction func addGroup2(_ sender: UIButton) {
-        ref = FIRDatabase.database().reference()
+        if buttonSelected["blackPink"]! {
+            secondButton.setBackgroundImage(#imageLiteral(resourceName: "g2"), for: UIControlState.normal)
+            buttonSelected["blackPink"] = false
+        } else {
+            secondButton.setBackgroundImage(#imageLiteral(resourceName: "check"), for: UIControlState.normal)
+            secondButton.backgroundColor = UIColor.clear
+            buttonSelected["blackPink"] = true
+        }
+        /*ref = FIRDatabase.database().reference()
         
         let kpopGroupRef = ref.child("users/" + userID! + "/kpopGroups")
         
@@ -112,12 +143,20 @@ class OnboardingVC: UIViewController {
             } else {
                 print("Error! Could not decode group data")
             }
-        })
+        })*/
 
     }
     
     @IBAction func addGroup3(_ sender: UIButton) {
-        ref = FIRDatabase.database().reference()
+        if buttonSelected["exo"]! {
+            thirdButton.setBackgroundImage(#imageLiteral(resourceName: "g3"), for: UIControlState.normal)
+            buttonSelected["exo"] = false
+        } else {
+            thirdButton.setBackgroundImage(#imageLiteral(resourceName: "check"), for: UIControlState.normal)
+            thirdButton.backgroundColor = UIColor.clear
+            buttonSelected["exo"] = true
+        }
+        /*ref = FIRDatabase.database().reference()
         
         let kpopGroupRef = ref.child("users/" + userID! + "/kpopGroups")
         
@@ -134,11 +173,19 @@ class OnboardingVC: UIViewController {
             } else {
                 print("Error! Could not decode group data")
             }
-        })
+        })*/
     }
     
     @IBAction func addGroup4(_ sender: UIButton) {
-        ref = FIRDatabase.database().reference()
+        if buttonSelected["twice"]! {
+            fourthButton.setBackgroundImage(#imageLiteral(resourceName: "g4"), for: UIControlState.normal)
+            buttonSelected["twice"] = false
+        } else {
+            fourthButton.setBackgroundImage(#imageLiteral(resourceName: "check"), for: UIControlState.normal)
+            fourthButton.backgroundColor = UIColor.clear
+            buttonSelected["twice"] = true
+        }
+        /*ref = FIRDatabase.database().reference()
         
         let kpopGroupRef = ref.child("users/" + userID! + "/kpopGroups")
         
@@ -155,11 +202,19 @@ class OnboardingVC: UIViewController {
             } else {
                 print("Error! Could not decode group data")
             }
-        })
+        })*/
     }
     
     @IBAction func addGroup5(_ sender: UIButton) {
-        ref = FIRDatabase.database().reference()
+        if buttonSelected["bigBang"]! {
+            fifthButton.setBackgroundImage(#imageLiteral(resourceName: "g5"), for: UIControlState.normal)
+            buttonSelected["bigBang"] = false
+        } else {
+            fifthButton.setBackgroundImage(#imageLiteral(resourceName: "check"), for: UIControlState.normal)
+            fifthButton.backgroundColor = UIColor.clear
+            buttonSelected["bigBang"] = true
+        }
+        /*ref = FIRDatabase.database().reference()
         
         let kpopGroupRef = ref.child("users/" + userID! + "/kpopGroups")
         
@@ -176,11 +231,19 @@ class OnboardingVC: UIViewController {
             } else {
                 print("Error! Could not decode group data")
             }
-        })
+        })*/
     }
     
     @IBAction func addGroup6(_ sender: UIButton) {
-        ref = FIRDatabase.database().reference()
+        if buttonSelected["got7"]! {
+            sixthButton.setBackgroundImage(#imageLiteral(resourceName: "g6"), for: UIControlState.normal)
+            buttonSelected["got7"] = false
+        } else {
+            sixthButton.setBackgroundImage(#imageLiteral(resourceName: "check"), for: UIControlState.normal)
+            sixthButton.backgroundColor = UIColor.clear
+            buttonSelected["got7"] = true
+        }
+        /*ref = FIRDatabase.database().reference()
         
         let kpopGroupRef = ref.child("users/" + userID! + "/kpopGroups")
         
@@ -197,13 +260,27 @@ class OnboardingVC: UIViewController {
             } else {
                 print("Error! Could not decode group data")
             }
-        })
+        })*/
+    }
+    
+    @IBAction func addGroups(_ sender: Any) {
+        let kpopGroupRef = self.userRef.child(userID! + "/kpopGroups")
+        
+        for (key, value) in buttonSelected {
+            if value {
+                if let groupID = self.groupIDs[key] {
+                    kpopGroupRef.child(key).setValue(groupID)
+                }
+            }
+        }
+        self.performSegue(withIdentifier: "showLanding", sender: UIButton.self)
     }
  
     func createGradientLayer() {
         gradientLayer = CAGradientLayer()
         gradientLayer.frame = self.view.bounds
-        gradientLayer.colors = [UIColor(red: 0.9804, green: 0.7882, blue: 0.4863, alpha: 1.0).cgColor, UIColor(red:0.92, green:0.56, blue:0.84, alpha:1.0).cgColor]
+        gradientLayer.colors = [UIColor(red: 0.9804, green: 0.7882, blue: 0.4863, alpha: 1.0).cgColor,
+                                UIColor(red:0.92, green:0.56, blue:0.84, alpha:1.0).cgColor]
         self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
